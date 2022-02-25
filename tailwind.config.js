282078d095f365b630ab80c87fedf229a834bd7f
Alagie Singhateh <43107984+singhateh@users.noreply.github.com>
2022-02-25 14:52:22 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const colors = require('tailwindcss/colors')
 module.exports = {
     content: [
         './resources/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
     ],
     theme: {
         extend: {
